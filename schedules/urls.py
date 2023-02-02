613bbffe2e17f232441ddf6d6a7d0f2e978a6281
puyan <p.behdarvandi@gmail.com>
2023-02-02 19:44:14 +0100

    Make some changes to Teachers and Students tabs

--- schedules/urls.py
+++ schedules/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from schedules.views import AvailableTimesView
+from schedules.views import AvailableTimesView, TeachersDetailView
 
 urlpatterns = [
+    path("teachers_detail/", TeachersDetailView.as_view(), name='teachers_detail'),
     path("time_schedules/<int:teacher_id>", AvailableTimesView.as_view(), name='availability'),
-
 ]
--- schedules/views.py
+++ schedules/views.py
@@ -5,10 +5,16 @@ from schedules.models import Schedule
 from users.models import Teacher
 
 
+class TeachersDetailView(View):
+    def get(self, request):
+        teachers = Teacher.objects.all()
+        context = {'teachers': teachers}
+        return render(request, "schedules/teachers_detail.html", context)
+
+
 class AvailableTimesView(View):
     def get(self, request, teacher_id):
         available_times = Schedule.objects.all().filter(id=teacher_id)
         teacher = Teacher.objects.get(id=teacher_id)
         context = {'available_times': available_times, 'teacher': teacher}
         return render(request, "schedules/time_schedules.html", context)
-
